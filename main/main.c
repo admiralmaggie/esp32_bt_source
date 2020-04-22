015a19bff49c8d8ca02528cf9bcf40512fec91fe
admiralmaggie <arouhi@abap.us>
2020-04-21 22:36:01 -0400

    bug fix 1

--- main/main.c
+++ main/main.c
@@ -173,7 +173,7 @@ void app_main(void)
     gpio_config_t io_conf;
 	io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
 	io_conf.mode = GPIO_MODE_INPUT;
-	io_conf.pin_bit_mask = GPIO_PIN_0;
+	io_conf.pin_bit_mask = (1ULL<<GPIO_PIN_0);
 	io_conf.pull_down_en = 0;
 	io_conf.pull_up_en = 0;
 	gpio_config(&io_conf);
